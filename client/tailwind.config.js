7c45de22bf458a1a3618ad419972a0bfd51bb2b5
Sujay V <sujaywebdev@gmail.com>
2023-02-15 12:11:56 +0530

    Basic Files and folder structure

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      epilogue: ["Epilogue", "sans-serif"],
+    },
+    boxShadow: {
+      secondary: "10px 10px 20px rgba(2, 2, 2, 0.25)",
+    },
+  },
+  plugins: [],
+};
